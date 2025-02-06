9120e3e50003ad9a138137016416efabcd6b762a
faith-in-het <157484919+faith-in-het@users.noreply.github.com>
2025-02-06 17:02:09 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -37,3 +37,5 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
+//slice-it doesnt manupulate the original array 
+//splice=it does separate the original array 
