c936b07e369677cc964498334ab38ac27daec300
faith-in-het <157484919+faith-in-het@users.noreply.github.com>
2025-02-08 11:28:00 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -9,4 +9,7 @@
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
+//second bracket is executing the function
 
+//why we are using IIFE : to reduce global scope pollution back in the days we were using var as a keyword and it was creating lot of issues so this is one of the solutions 
+//using le and const also a solution 
